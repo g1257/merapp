de7c6f11ca27dba90ca39ac34c62e1055ef0319d
Gonzalo Alvarez <gz1@ornl.gov>
2016-09-08 11:51:57 -0500

    free indices

--- src/Engine/MeraToTikz.h
+++ src/Engine/MeraToTikz.h
@@ -10,7 +10,13 @@ class MeraToTikz {
 
 	enum TensorTypeEnum {TENSOR_TYPE_UNKNOWN,TENSOR_TYPE_W,TENSOR_TYPE_U};
 
+	enum IndexDirectionEnum {INDEX_DIR_IN, INDEX_DIR_OUT};
+
+	enum IndexTypeEnum {INDEX_TYPE_SUMMED, INDEX_TYPE_FREE};
+
 	typedef PsimagLite::Vector<PsimagLite::String>::Type VectorStringType;
+	typedef std::pair<char,SizeType> PairCharSizeType;
+	typedef PsimagLite::Vector<PairCharSizeType>::Type VectorPairCharSizeType;
 
 	class OpaqueTensor {
 
@@ -48,23 +54,62 @@ class MeraToTikz {
 
 			timeIndex_ = atoi(tokens[1].c_str());
 			spaceIndex_ = atoi(tokens[2].c_str());
-			ins_ = atoi(tokens[3].c_str());
-			outs_ = atoi(tokens[4].c_str());
+			SizeType ins = atoi(tokens[3].c_str());
+			SizeType outs = atoi(tokens[4].c_str());
 
-			if (ins_ + outs_ + 5 != ts) {
+			if (ins + outs + 5 != ts) {
 				PsimagLite::String str("OpaqueTensor: malformed partial srep ");
 				throw PsimagLite::RuntimeError(str + srep_ + "\n");
 			}
+
+			for (SizeType i = 0; i < ins; ++i) {
+				PsimagLite::String item = tokens[5+i];
+				l = item.length();
+				if (l == 0) {
+					PsimagLite::String str("OpaqueTensor: malformed srep, in s-index ");
+					throw PsimagLite::RuntimeError(str + ttos(i) + ", " + srep_ + "\n");
+				}
+
+				if (l == 1 && item != "d") {
+					PsimagLite::String str("OpaqueTensor: malformed srep, in s-index ");
+					throw PsimagLite::RuntimeError(str + ttos(i) + ", " + srep_ + "\n");
+				}
+
+				PsimagLite::String tmp = item.substr(1,l);
+				insSi_.push_back(PairCharSizeType(item[0],atoi(tmp.c_str())));
+			}
+
+			for (SizeType i = 0; i < outs; ++i) {
+				PsimagLite::String item = tokens[5+ins+i];
+				l = item.length();
+
+				if (l == 0) {
+					PsimagLite::String str("OpaqueTensor: malformed srep, out s-index ");
+					throw PsimagLite::RuntimeError(str + ttos(i) + ", " + srep_ + "\n");
+				}
+
+				if (l == 1 && item != "d") {
+					PsimagLite::String str("OpaqueTensor: malformed srep, out s-index ");
+					throw PsimagLite::RuntimeError(str + ttos(i) + ", " + srep_ + "\n");
+				}
+
+				PsimagLite::String tmp = (l == 1) ? "0" : item.substr(1,l);
+				outsSi_.push_back(PairCharSizeType(item[0],atoi(tmp.c_str())));
+			}
 		}
 
-		SizeType x() const
-		{
-			return spaceIndex_;
-		}
+		SizeType x() const { return spaceIndex_; }
 
-		SizeType y() const
+		SizeType y() const { return timeIndex_; }
+
+		SizeType ins() const { return insSi_.size(); }
+
+		SizeType outs() const { return outsSi_.size(); }
+
+		IndexTypeEnum indexType(SizeType ind, IndexDirectionEnum dir) const
 		{
-			return timeIndex_;
+			char c = (dir == INDEX_DIR_IN) ? insSi_[ind].first : outsSi_[ind].first;
+			return (c == 's') ? INDEX_TYPE_SUMMED : INDEX_TYPE_FREE;
 		}
 
 		TensorTypeEnum type() const { return type_; }
@@ -73,12 +118,12 @@ class MeraToTikz {
 
 		SizeType timeIndex_;
 		SizeType spaceIndex_;
-		SizeType ins_;
-		SizeType outs_;
 		bool conjugate_;
 		PsimagLite::String srep_;
 		PsimagLite::String name_;
 		TensorTypeEnum type_;
+		VectorPairCharSizeType insSi_;
+		VectorPairCharSizeType outsSi_;
 	};
 
 public:
@@ -143,26 +188,41 @@ private:
 		for (SizeType i = 0; i < tensor_.size(); ++i) {
 
 			RealType xsep = 1.5*(1.0+tensor_[i]->y());
-			RealType x = xsep*tensor_[i]->x() + 1.5*tensor_[i]->y();
-			RealType y = 3.5*tensor_[i]->y();
-			RealType x2 = x + 1;
-			RealType y2 = y + 1.5;
+			RealType xdisen = xsep*tensor_[i]->x() + 1.5*tensor_[i]->y();
+			RealType ydisen = 3.5*tensor_[i]->y();
+			RealType xisom = xdisen + 1;
+			RealType yisom = ydisen + 1.5;
+			SizeType ins = tensor_[i]->ins();
 			if (tensor_[i]->type() == TENSOR_TYPE_U) {
-				buffer_ += ("\\coordinate (A");
-				buffer_ += ttos(i) + ") at (" + ttos(x) + "," + ttos(y) + ");\n";
-				buffer_ += ("\\coordinate (B");
-				buffer_ += ttos(i) + ") at (" + ttos(x+dx);
-				buffer_ += "," + ttos(y+dy) + ");\n";
+				buffer_ += "\\coordinate (A";
+				buffer_ += ttos(i) + ") at (" + ttos(xdisen) + "," + ttos(ydisen) + ");\n";
+				buffer_ += "\\coordinate (B";
+				buffer_ += ttos(i) + ") at (" + ttos(xdisen+dx);
+				buffer_ += "," + ttos(ydisen+dy) + ");\n";
 				buffer_ += "\\draw[disen] (A" + ttos(i) + ") rectangle (B";
 				buffer_ += ttos(i)+ ");\n";
+				// ins
+				assert(ins > 0);
+				RealType a = dx/(ins - 1);
+				for (SizeType j = 0; j < ins; ++j) {
+					RealType xtmp = a*j + xdisen;
+					buffer_ += "\\coordinate (I";
+					buffer_ += ttos(i) + ") at (" + ttos(xtmp) + "," + ttos(ydisen) + ");\n";
+					if (tensor_[i]->indexType(j,INDEX_DIR_IN) == INDEX_TYPE_FREE) {
+						buffer_ += ("\\coordinate (IF");
+						buffer_ += ttos(i) + ") at (" + ttos(xtmp) + ",";
+						buffer_ += ttos(ydisen-0.5*dy) + ");\n";
+						buffer_ += "\\draw (IF" + ttos(i) + ") circle (0.2);\n";
+					}
+				}
 			} else {
-				buffer_ += ("\\coordinate (A");
-				buffer_ += ttos(i) + ") at (" + ttos(x2) + "," + ttos(y2) + ");\n";
-				buffer_ += ("\\coordinate (B");
-				buffer_ += ttos(i) + ") at (" + ttos(x2+dx);
-				buffer_ += "," + ttos(y2) + ");\n";
-				buffer_ += ("\\coordinate (C");
-				buffer_ += ttos(i) + ") at (" + ttos(x2+0.5*dx) + "," + ttos(y2+dy) + ");\n";
+				buffer_ += "\\coordinate (A";
+				buffer_ += ttos(i) + ") at (" + ttos(xisom) + "," + ttos(yisom) + ");\n";
+				buffer_ += "\\coordinate (B";
+				buffer_ += ttos(i) + ") at (" + ttos(xisom+dx);
+				buffer_ += "," + ttos(yisom) + ");\n";
+				buffer_ += "\\coordinate (C";
+				buffer_ += ttos(i) + ") at (" + ttos(xisom+0.5*dx) + "," + ttos(yisom+dy) + ");\n";
 				buffer_ += "\\draw[isom] (A" + ttos(i) + ") -- (B" + ttos(i) + ") -- ";
 				buffer_ += "(C" + ttos(i) + ") -- cycle;\n";
 			}
