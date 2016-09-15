2b78b5cdd433eb748fdd203e01afc36f040bc0af
Gonzalo Alvarez <gz1@ornl.gov>
2016-09-15 15:20:55 -0500

    working on tensor optimizer

--- src/Engine/TensorOptimizer.h
+++ src/Engine/TensorOptimizer.h
@@ -4,14 +4,35 @@
 #include "IoSimple.h"
 #include "TensorSrep.h"
 #include "TensorEval.h"
+#include <algorithm>
+#include "Sort.h"
 
 namespace Mera {
+
+bool operator==(const std::pair<PsimagLite::String,SizeType>& p1,
+                const std::pair<PsimagLite::String,SizeType>& p2)
+{
+	if (p1.first != p2.first) return false;
+	if (p1.second != p2.second) return false;
+	return true;
+}
+
+bool operator!=(const std::pair<PsimagLite::String,SizeType>& p1,
+                const std::pair<PsimagLite::String,SizeType>& p2)
+{
+	if (p1.first != p2.first) return true;
+	if (p1.second != p2.second) return true;
+	return false;
+}
 
 template<typename ComplexOrRealType>
 class TensorOptimizer {
 
 	//typedef PsimagLite::Vector<PsimagLite::String>::Type VectorStringType;
+	typedef PsimagLite::Vector<SizeType>::Type VectorSizeType;
 	typedef PsimagLite::Vector<TensorSrep*>::Type VectorTensorSrepType;
+	typedef std::pair<PsimagLite::String,SizeType> PairStringSizeType;
+	typedef PsimagLite::Vector<PairStringSizeType>::Type VectorPairStringSizeType;
 
 public:
 
@@ -20,20 +41,29 @@ public:
 	typedef TensorEvalType::TensorType TensorType;
 	typedef TensorEvalType::VectorTensorType VectorTensorType;
 
-	TensorOptimizer(IoInType& io, PsimagLite::String name, SizeType id)
+	TensorOptimizer(IoInType& io,
+	                PsimagLite::String nameToOptimize,
+	                SizeType idToOptimize)
 	{
 		SizeType terms = 0;
 		io.readline(terms,"TERMS=");
-		std::cerr<<"Read "<<terms<<" for tensor id "<<id<<"\n";
+		std::cerr<<"Read "<<terms<<" for tensor id "<<idToOptimize<<"\n";
 		tensorSrep_.resize(terms,0);
 		for (SizeType i = 0; i < terms; ++i) {
 			PsimagLite::String srep;
 			io.readline(srep,"STRING=");
 			std::cerr<<"Read string "<<srep<<"\n";
 			tensorSrep_[i] = new TensorSrep(srep);
-			findTensors(*(tensorSrep_[i]));
+			findTensors(*(tensorSrep_[i]),nameToOptimize,idToOptimize);
 		}
 
+		PsimagLite::Sort<VectorPairStringSizeType> sort;
+		VectorSizeType perm(tensorNameIds_.size(),0);
+		sort.sort(tensorNameIds_,perm);
+		SizeType end = (std::unique(tensorNameIds_.begin(),
+		                            tensorNameIds_.end()) -
+		                tensorNameIds_.begin());
+		tensorNameIds_.resize(end);
 		std::cerr<<"TensorOptimizer::ctor() done\n";
 	}
 
@@ -51,9 +81,21 @@ public:
 
 private:
 
-	void findTensors(const TensorSrep& t)
+	void findTensors(const TensorSrep& t,
+	                 PsimagLite::String nameToOptimize,
+	                 SizeType idToOptimize)
 	{
-
+		SizeType ntensors = t.size();
+		for (SizeType i = 0; i < ntensors; ++i) {
+			PsimagLite::String name = t(i).name();
+			SizeType id = t(i).id();
+			bool conjugate = t(i).isConjugate();
+			bool b = (name == nameToOptimize && id == idToOptimize && conjugate);
+			if (!b && conjugate) continue;
+			if (name == "r") continue;
+			PairStringSizeType p(name,id);
+			tensorNameIds_.push_back(p);
+		}
 	}
 
 	TensorOptimizer(const TensorOptimizer&);
@@ -62,6 +104,7 @@ private:
 
 	VectorTensorSrepType tensorSrep_;
 	VectorTensorType tensors_;
+	VectorPairStringSizeType tensorNameIds_;
 }; // class TensorOptimizer
 } // namespace Mera
 #endif // TENSOROPTIMIZER_H
