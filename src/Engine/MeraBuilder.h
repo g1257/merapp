d9ff853d2a1658af8acc5fd825b429f35a02b720
Gonzalo Alvarez <gz1@ornl.gov>
2016-12-06 18:32:21 -0500

    MeraBuilder: works

--- src/Engine/MeraBuilder.h
+++ src/Engine/MeraBuilder.h
@@ -21,16 +21,22 @@ public:
 			throw PsimagLite::RuntimeError("MeraBuilder: sites must be a power of 2\n");
 
 		SizeType tensors = sites/2;
-		srep_ = "ln=" + ttos(ln) + "\n";
 		SizeType counter = 0;
+		SizeType summed = 0;
+		SizeType savedSummedForU = 0;
+		SizeType idsU = 0;
+		SizeType idsW = 0;
 		while (tensors > 1) {
-			createUlayer(tensors,counter);
-			createWlayer(tensors,counter);
+			SizeType savedSummedForW = summed;
+			createUlayer(summed,idsU,savedSummedForU,tensors,counter);
+			savedSummedForU = summed;
+			createWlayer(summed,idsW,savedSummedForW,tensors,counter);
 			tensors /= 2;
 			counter++;
 		}
 
-		srep_ += "r(,)";
+		assert(summed > 1);
+		srep_ += "r(s" + ttos(summed-2) + ",s" + ttos(summed-1) + ")";
 	}
 
 	const PsimagLite::String& operator()() const
@@ -40,7 +46,11 @@ public:
 
 private:
 
-	void createUlayer(SizeType n, SizeType counter)
+	void createUlayer(SizeType& summed,
+	                  SizeType& idsU,
+	                  SizeType savedSummed,
+	                  SizeType n,
+	                  SizeType counter)
 	{
 		PsimagLite::String I0("");
 		PsimagLite::String I1("");
@@ -51,6 +61,9 @@ private:
 			if (counter == 0) {
 				I0 = "f" + ttos(2*i);
 				I1 = "f" + ttos(2*i+1);
+			} else {
+				I0 = "s" + ttos(savedSummed++);
+				I1 = "s" + ttos(savedSummed++);
 			}
 
 			if (counter & 1) {
@@ -59,16 +72,45 @@ private:
 				if (i == 0) hasTwoOutputs = false;
 			}
 
-			srep_ += "u("+I0+"," + I1 + "|" + O0;
-			if (hasTwoOutputs) srep_ += "," + O1;
+			O0 = "s" + ttos(summed++);
+			srep_ += "u" + ttos(idsU++) + "("+I0+"," + I1 + "|" + O0;
+			if (hasTwoOutputs) {
+				O1 = "s" + ttos(summed++);
+				srep_ += "," + O1;
+			}
+
 			srep_ += ")";
 		}
 	}
 
-	void createWlayer(SizeType n, SizeType counter)
+	void createWlayer(SizeType& summed,
+	                  SizeType& idsW,
+	                  SizeType savedSummed,
+	                  SizeType n,
+	                  SizeType counter)
 	{
-		for (SizeType i = 0; i < n; ++i)
-			srep_ += "w(,|)";
+		PsimagLite::String I0("");
+		PsimagLite::String I1("");
+		PsimagLite::String O0("");
+		for (SizeType i = 0; i < n; ++i) {
+			bool hasTwoInputs = true;
+
+			if (counter & 1) {
+				if (i == 0) hasTwoInputs = false;
+			} else {
+				if (i + 1 == n) hasTwoInputs = false;
+			}
+
+			I0 = "s" + ttos(savedSummed++);
+			srep_ += "w" + ttos(idsW++) +"(" + I0;
+			if (hasTwoInputs) {
+				I1 = "s" + ttos(savedSummed++);
+				srep_ += "," + I1;
+			}
+
+			O0 = "s" + ttos(summed++);
+			srep_ += "|" + O0 + ")";
+		}
 	}
 
 	SizeType logBase2Strict(SizeType x) const
