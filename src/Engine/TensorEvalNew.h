8138ba9b6c075c7021d063dd41e5fa15ef22bec0
Gonzalo Alvarez <gz1@ornl.gov>
2017-01-11 15:29:01 -0500

    added tensor eval new

---
+++ src/Engine/TensorEvalNew.h
@@ -0,0 +1,29 @@
+/*
+Copyright (c) 2016-2017, UT-Battelle, LLC
+
+MERA++, Version 0.
+
+This file is part of MERA++.
+MERA++ is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+MERA++ is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with MERA++. If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef TENSOREVALNEW_H
+#define TENSOREVALNEW_H
+#include "TensorEvalBase.h"
+
+namespace Mera {
+
+template<typename ComplexOrRealType>
+class TensorEvalNew : public TensorEvalBase<ComplexOrRealType> {
+
+};
+} // namespace Mera
+#endif // TENSOREVALNEW_H
