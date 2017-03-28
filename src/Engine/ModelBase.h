85f3159136d51e7beb5e899e40a491463534c0a1
Gonzalo Alvarez <gz1@ornl.gov>
2017-03-28 10:31:54 -0400

    ModelBase

---
+++ src/Engine/ModelBase.h
@@ -0,0 +1,44 @@
+/*
+Copyright (c) 2016-2017, UT-Battelle, LLC
+
+MERA++, Version 0.
+
+This file is part of MERA++.
+MERA++ is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+MERA++ is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with MERA++. If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef MERA_MODEL_BASE
+#define MERA_MODEL_BASE
+#include "Matrix.h"
+#include "Vector.h"
+
+namespace  Mera {
+
+template<typename ComplexOrRealType_>
+class ModelBase {
+
+public:
+
+	typedef ComplexOrRealType_ ComplexOrRealType;
+	typedef typename PsimagLite::Vector<ComplexOrRealType>::Type VectorType;
+	typedef typename PsimagLite::Real<ComplexOrRealType>::Type RealType;
+	typedef typename PsimagLite::Vector<RealType>::Type VectorRealType;
+	typedef PsimagLite::Matrix<ComplexOrRealType> MatrixType;
+	typedef typename PsimagLite::Vector<MatrixType*>::Type VectorMatrixType;
+
+	virtual ~ModelBase() {}
+
+	virtual const RealType& energyShift() const = 0;
+
+	virtual const MatrixType& twoSiteHam(SizeType id) const = 0;
+};
+}
+#endif // HEISENBERG_H
---
+++ src/Engine/ModelSelector.h
@@ -0,0 +1,73 @@
+/*
+Copyright (c) 2016-2017, UT-Battelle, LLC
+
+MERA++, Version 0.
+
+This file is part of MERA++.
+MERA++ is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+MERA++ is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with MERA++. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef MERA_MODEL_SELECTOR_H
+#define MERA_MODEL_SELECTOR_H
+#include "../Models/Heisenberg/Heisenberg.h"
+
+namespace Mera {
+
+template<typename ModelBaseType>
+class ModelSelector {
+
+	typedef typename ModelBaseType::VectorType VectorType;
+
+	// start models here:
+	typedef Heisenberg<ModelBaseType> ModelHeisenbergType;
+	// end models
+
+public:
+
+	ModelSelector(const PsimagLite::String& name, const VectorType& hTerms)
+	    : name_(name),model_(0)
+	{
+		if (name_ == "Heisenberg") {
+			model_ = new ModelHeisenbergType(hTerms);
+		} else {
+			PsimagLite::String s(__FILE__);
+			s += " Unknown model " + name_ + "\n";
+			throw PsimagLite::RuntimeError(s.c_str());
+		}
+	}
+
+	~ModelSelector()
+	{
+		if (model_) delete model_;
+	}
+
+	const ModelBaseType& operator()()
+	{
+		return *model_;
+	}
+
+private:
+
+	ModelSelector(const ModelSelector&);
+
+	ModelSelector& operator=(const ModelSelector&);
+
+	PsimagLite::String name_;
+	ModelBaseType* model_;
+
+}; // ModelSelector
+
+} // namespace Mera
+
+/*@}*/
+#endif // MERA_MODEL_SELECTOR_H
+
